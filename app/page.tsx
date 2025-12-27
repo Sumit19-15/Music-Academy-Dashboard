eb17a3358fed69e87e8ac69810606e4120c3a466
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2025-12-27 20:44:35 +0530

    ui error more resolve

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import { main } from "motion/react-client";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-
-    </main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]"></main>
   );
 }
