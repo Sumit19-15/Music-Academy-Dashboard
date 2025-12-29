5f75bac10cb639d1da93212c29c29407d3cf5aeb
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2025-12-29 16:27:07 +0530

    adding the infinite cards section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import GridBackgroundCards from "@/components/GridBackgroundCards";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <HeroSection></HeroSection>
       <FeaturedCourses></FeaturedCourses>
       <WhyChooseUs></WhyChooseUs>
+      <GridBackgroundCards></GridBackgroundCards>
     </main>
   );
 }
