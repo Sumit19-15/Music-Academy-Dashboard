72bc6dfdc6b28b3cc0461190ca14f72ac5eed77f
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2025-12-29 15:16:47 +0530

    adding the scrolling steaky reveal section from the ui library

---
+++ components/WhyChooseUs.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+
+const musicSchoolContent = [
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that's uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+  },
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that's uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+  },
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that's uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+  },
+  {
+    title: "Live Feedback & Engagement",
+    description:
+      "Immerse yourself in an interactive learning experience where feedback is immediate, just like real-time changes in a collaborative project. This approach enhances your understanding and mastery of music concepts and performance techniques.",
+  },
+  {
+    title: "Cutting-Edge Curriculum",
+    description:
+      "Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you're always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.",
+  },
+  {
+    title: "Limitless Learning Opportunities",
+    description:
+      "With our expansive resource library and dynamic course offerings, you'll never find yourself without something new to explore. Our platform provides continuous opportunities for growth, ensuring your musical skills are always advancing.",
+  },
+  //   {
+  //     title: "Real time changes",
+  //     description:
+  //       "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+  //     content: (
+  //       <div className="flex h-full w-full items-center justify-center text-white">
+  //         <img
+  //           src="https://media.istockphoto.com/id/912251752/photo/young-musician-playing-acoustic-guitar-close-up.jpg?s=612x612&w=0&k=20&c=NKroV7I57kxHaEBhE1G50TAwuMgMvOHKTeYyZskqWw4="
+  //           width={300}
+  //           height={300}
+  //           className="h-full w-full object-cover"
+  //           alt="linear board demo"
+  //         />
+  //       </div>
+  //     ),
+  //   }
+];
+
+function WhyChooseUs() {
+  return (
+    <div>
+      <StickyScroll content={musicSchoolContent} />
+    </div>
+  );
+}
+
+export default WhyChooseUs;
